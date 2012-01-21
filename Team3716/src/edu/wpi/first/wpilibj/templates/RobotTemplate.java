378a0f5be535a198813aec610d257c4a04172e86
Colin Feeney <colin.feeney@wastudent.org>
2012-01-21 12:30:21 -0700

    First Upload

--- Team3716/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ Team3716/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -9,6 +9,7 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.templates.commands.CommandBase;
